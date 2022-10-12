eb96fa43b2b7c8c3113da187656f1a47518899d3
okumura.hideki <okumurahidekikangeki@gmail.com>
2022-10-12 16:05:25 +0000

    change cpu's metrics

--- cpu/profile.cpp
+++ cpu/profile.cpp
@@ -8,7 +8,7 @@
 using time_ms_t = int64_t;
 
 template <typename T, typename F>
-time_ms_t _mmmul_profile(const usize M, const usize N, const usize K, F mmmul) {
+double _mmmul_profile(const usize M, const usize N, const usize K, F mmmul) {
   auto a = std::make_unique<T[]>(M * K);
   auto b = std::make_unique<T[]>(K * N);
   auto c = std::make_unique<T[]>(M * N);
@@ -16,17 +16,21 @@ time_ms_t _mmmul_profile(const usize M, const usize N, const usize K, F mmmul) {
   for (int i = 0; i < M * K; i++) a[i] = i % 1024 - 512;
   for (int i = 0; i < K * N; i++) b[i] = i % 1024 - 512;
 
+  const int m = 50;
+
   auto start = std::chrono::system_clock::now();
-  for (int i = 0; i < 50; i++) mmmul(M, N, K, a.get(), b.get(), c.get());
+  for (int i = 0; i < m; i++) mmmul(M, N, K, a.get(), b.get(), c.get());
   auto time_obj = std::chrono::system_clock::now() - start;
   time_ms_t time =
       std::chrono::duration_cast<std::chrono::milliseconds>(time_obj).count();
 
-  return time;
+  const double op_Gn = M * N * K * m / 1e9;
+
+  return op_Gn / (time / 1000.0);
 }
 
 template <typename T, typename F>
-time_ms_t _mvmul_profile(const usize M, const usize K, F mvmul) {
+double _mvmul_profile(const usize M, const usize K, F mvmul) {
   auto a = std::make_unique<T[]>(M * K);
   auto b = std::make_unique<T[]>(K);
   auto c = std::make_unique<T[]>(M);
@@ -34,28 +38,31 @@ time_ms_t _mvmul_profile(const usize M, const usize K, F mvmul) {
   for (int i = 0; i < M * K; i++) a[i] = i % 1024 - 512;
   for (int i = 0; i < K; i++) b[i] = i % 1024 - 512;
 
+  const int m = 50;
+
   auto start = std::chrono::system_clock::now();
-  for (int i = 0; i < 50; i++) mvmul(M, K, a.get(), b.get(), c.get());
+  for (int i = 0; i < m; i++) mvmul(M, K, a.get(), b.get(), c.get());
   auto time_obj = std::chrono::system_clock::now() - start;
   time_ms_t time =
       std::chrono::duration_cast<std::chrono::milliseconds>(time_obj).count();
+  const double op_Gn = M * K * m / 1e9;
 
-  return time;
+  return op_Gn / (time / 1000.0);
 }
 
 template <typename T, typename F>
-time_ms_t mmmul_profile(const usize M, const usize N, const usize K, F f) {
+double mmmul_profile(const usize M, const usize N, const usize K, F f) {
   return _mmmul_profile<T>(M, N, K, std::function<mmmul_t<T>>(f));
 }
 template <typename T, typename F>
-time_ms_t mvmul_profile(const usize M, const usize K, F f) {
+double mvmul_profile(const usize M, const usize K, F f) {
   return _mvmul_profile<T>(M, K, std::function<mvmul_t<T>>(f));
 }
 
-#define timeit(exp)                                     \
-  {                                                     \
-    time_ms_t time = (exp);                             \
-    std::cout << time << "(ms): " << #exp << std::endl; \
+#define gflopsit(exp)                                         \
+  {                                                           \
+    double gflops = (exp);                                    \
+    std::cout << gflops << "(GFlops): " << #exp << std::endl; \
   }
 
 int main() {
@@ -63,10 +70,10 @@ int main() {
   {
     const usize N = 256;
 #define do_short(target) \
-  timeit(mmmul_profile<short>(N, N, N, target::mmmul<short>))
-#define do_int(target) timeit(mmmul_profile<int>(N, N, N, target::mmmul<int>))
+  gflopsit(mmmul_profile<short>(N, N, N, target::mmmul<short>))
+#define do_int(target) gflopsit(mmmul_profile<int>(N, N, N, target::mmmul<int>))
 #define do_float(target) \
-  timeit(mmmul_profile<float>(N, N, N, target::mmmul<float>))
+  gflopsit(mmmul_profile<float>(N, N, N, target::mmmul<float>))
 
     std::cout << "--- mmmul( Square matrix ) N=" << N << std::endl;
 
@@ -98,10 +105,10 @@ int main() {
   {
     const usize N = 1024;
 #define do_short(target) \
-  timeit(mvmul_profile<short>(N, N, target::mvmul<short>))
-#define do_int(target) timeit(mvmul_profile<int>(N, N, target::mvmul<int>))
+  gflopsit(mvmul_profile<short>(N, N, target::mvmul<short>))
+#define do_int(target) gflopsit(mvmul_profile<int>(N, N, target::mvmul<int>))
 #define do_float(target) \
-  timeit(mvmul_profile<float>(N, N, target::mvmul<float>))
+  gflopsit(mvmul_profile<float>(N, N, target::mvmul<float>))
 
     std::cout << "--- mvmul( Square matrix ) N=" << N << std::endl;
 
